d90466417aa60a9c8bedecb7a115bdcde27ebddc
nati2331 <natnael.mk2@gmail.com>
2023-11-20 09:27:50 -0800

    sevesss

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,7 +2,7 @@
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-        return True
-    except (ValueError, TypeError):
-        print("Exception: Invalid value for integer", file=sys.stderr)
-        return False
+        return (True)
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (False)
