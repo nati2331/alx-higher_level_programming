29a3ff8cfd9f15444cae6cb74e7c3557065ed144
nati2331 <natnael.mk2@gmail.com>
2023-11-21 13:35:12 -0800

    102

---
+++ 0x06-python-classes/102-main.py
@@ -0,0 +1,19 @@
+guillaume@ubuntu:~/0x06$ cat 102-main.py
+#!/usr/bin/python3
+Square = __import__('102-square').Square
+
+s_5 = Square(5)
+s_6 = Square(6)
+
+if s_5 < s_6:
+    print("Square 5 < Square 6")
+if s_5 <= s_6:
+    print("Square 5 <= Square 6")
+if s_5 == s_6:
+    print("Square 5 == Square 6")
+if s_5 != s_6:
+    print("Square 5 != Square 6")
+if s_5 > s_6:
+    print("Square 5 > Square 6")
+if s_5 >= s_6:
+    print("Square 5 >= Square 6")
---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""creating a class"""
+
+class Square:
+    """class square created"""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """"measures length of side
+        Raises:
+            ValueErrorr: if size IS negative
+            TypeError: if size ISNOT INT
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """Computes area"""
+        return self.__size * self.__size
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __eq__(self, other):
+        return self.area() == other.area()
