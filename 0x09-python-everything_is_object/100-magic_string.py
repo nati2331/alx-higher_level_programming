61a1c63df456fe5314e370ea3c8c567aa6bce1ce
nati2331 <natnael.mk2@gmail.com>
2023-11-28 14:53:20 -0800

    lasts

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def magic_string(l=None):
+    if l is None:
+        l = []
+    l += ["BestSchool"]
+    return ", ".join(l)
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Defining a locked lass
+"""
+
+class LockedClass():
+
+"""
+Preventing dynamic attribute creation
+_slots_= ['first_name']
+"""
+
+    def __init__(self):
+
+        pass
+
